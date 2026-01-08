101ca7dce76577f95407f901bd9434d466530d44
Olivia <oliviawolfie@pm.me>
2026-01-08 01:09:01 +0100

    Switch alui to conan for deps

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+import os
+from conan.tools.files import copy
+from pathlib import Path
+import shutil
+
+class AluiTests(ConanFile):
+    """
+    Used for fetching dependencies for use in tests
+    """
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("freetype/2.13.3")
+        self.requires("libpng/1.6.53")
+        self.requires("zlib/1.3.1")
