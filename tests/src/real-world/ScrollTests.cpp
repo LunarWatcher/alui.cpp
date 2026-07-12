318ba592261552502b0bade11dae1356210c6ee1
Olivia <oliviawolfie@pm.me>
2026-07-12 03:09:03 +0200

    Add tests for nested scrolling

--- tests/src/real-world/ScrollTests.cpp
+++ tests/src/real-world/ScrollTests.cpp
@@ -1,3 +1,4 @@
+#include "alui/component/FlexDirection.hpp"
 #include "alui/components/Text.hpp"
 #include "alui/layouts/FlexBox.hpp"
 #include "fixtures/SetupGUI.hpp"
@@ -101,6 +102,164 @@ TEST_CASE("Test basic scroll functionality") {
         // Since we're at the bottom, further scrolling should be noop
         REQUIRE_FALSE(layout->scrollY(test::scroll(test::ScrollDirection::DOWN)));
     }
+}
+
+TEST_CASE("Scrolling an inner layout in an outer scrollable layout") {
+    test::SetupGUI setup;
+    auto rootLayout = std::make_shared<alui::FlexBox>(
+        alui::FlexDirection::Horizontal,
+        alui::ComponentConfig {
+            // aggressively constrain size to provoke scrolling
+            .minWidth = alui::Magnitude::Absolute(500.f),
+            .minHeight = alui::Magnitude::Absolute(500.f),
+            .maxWidth = alui::Magnitude::Absolute(500.f),
+            .maxHeight = alui::Magnitude::Absolute(500.f),
+            .style = test::getDebugStyleSpec(),
+        }
+    );
+    auto text = std::make_shared<alui::Text>(
+        test::getWordSoup(50),
+        alui::ComponentConfig {}
+    );
+    text->setTextColour(al_map_rgb(255, 255, 255));
+    rootLayout->push(text);
+
+    // The nested layout is constrained in the same way the root layout is, but with a smaller height. This is to allow
+    // both layout levels to be visible at the same time
+    auto nestedLayout = std::make_shared<alui::FlexBox>(
+        alui::FlexDirection::Horizontal,
+        alui::ComponentConfig {
+            .minWidth = alui::Magnitude::Absolute(500.f),
+            .minHeight = alui::Magnitude::Absolute(250.f),
+            .maxWidth = alui::Magnitude::Absolute(500.f),
+            .maxHeight = alui::Magnitude::Absolute(250.f),
+            .style = test::getDebugStyleSpec(),
+        }
+    );
+    auto nestedText = std::make_shared<alui::Text>(
+        test::getWordSoup(50),
+        alui::ComponentConfig {}
+    );
+    nestedText->setTextColour(al_map_rgb(255, 255, 255));
+    nestedLayout->push(nestedText);
+    rootLayout->push(nestedLayout);
+
+    setup->push(rootLayout);
+    setup.autoCapture(
+        "NestedScrollPreScroll.bmp"
+    );
+
+    REQUIRE(text->getComputedHeight() > 500);
+    REQUIRE(text->getComputedWidth() == 500);
+    REQUIRE(nestedText->getComputedHeight() > 500);
+    REQUIRE(nestedText->getComputedWidth() == 500);
+    REQUIRE(rootLayout->getScrollY() == 0);
+    REQUIRE(nestedLayout->getScrollY() == 0);
+
+    REQUIRE(nestedLayout->getComputedHeight() == 250);
+    REQUIRE(nestedLayout->getComputedWidth() == 500);
+
+    SECTION("Scroll control should transfer to the inner layout") {
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    1,
+                    test::ScrollDirection::DOWN,
+                    10000
+                )
+        );
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    1,
+                    test::ScrollDirection::UP,
+                    1
+                )
+        );
+        setup.autoCapture(
+            "NestedScrollPostOuterScroll.bmp"
+        );
+
+        REQUIRE(nestedLayout->getScrollY() == 0);
+        REQUIRE(rootLayout->getScrollY() == (
+                text->getComputedHeight() + nestedLayout->getComputedHeight()
+                // Subtract 1 screen height from the up event
+                - 72
+                - rootLayout->getComputedHeight()
+        ));
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    351,
+                    test::ScrollDirection::DOWN,
+                    10000
+                )
+        );
+        setup.autoCapture(
+            "NestedScrollPostInnerScroll.bmp"
+        );
+        REQUIRE(nestedLayout->getScrollY() == nestedText->getComputedHeight() - nestedLayout->getComputedHeight());
+        REQUIRE(rootLayout->getScrollY() == (
+                text->getComputedHeight() + nestedLayout->getComputedHeight()
+                // Subtract 1 screen height from the up event
+                - 72
+                - rootLayout->getComputedHeight()
+        ));
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    351,
+                    test::ScrollDirection::DOWN,
+                    10000
+                )
+        );
+        {
+            INFO(
+                "Scroll events in the nested layout should propagate to the outer layout when it can't scroll further down"
+            );
+            REQUIRE(nestedLayout->getScrollY() == nestedText->getComputedHeight() - nestedLayout->getComputedHeight());
+            REQUIRE(rootLayout->getScrollY() == (
+                    text->getComputedHeight() + nestedLayout->getComputedHeight()
+                    - rootLayout->getComputedHeight()
+                ));
+        }
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    351,
+                    test::ScrollDirection::UP,
+                    10000
+                )
+        );
+        REQUIRE(nestedLayout->getScrollY() == 0);
+        REQUIRE(rootLayout->getScrollY() == (
+                text->getComputedHeight() + nestedLayout->getComputedHeight()
+                - rootLayout->getComputedHeight()
+            ));
+
+        setup->handleEvent(
+            test::EventGenerator<ALLEGRO_EVENT_MOUSE_AXES>(setup)
+                .asScrollEvent(
+                    1,
+                    351,
+                    test::ScrollDirection::UP,
+                    10000
+                )
+        );
+        {
+            INFO(
+                "A second up event when the inner layout can't go up anymore should propagate to the other layout"
+            );
+            REQUIRE(nestedLayout->getScrollY() == 0);
+            REQUIRE(rootLayout->getScrollY() == 0);
+        }
+    }
+
 }
 
 }
